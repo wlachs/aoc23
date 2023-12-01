91e1cfedddad53599d18dc2076862a00e213a0ac
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-12-01 10:22:08 +0100

    implement part 1

--- days/day_01/main.go
+++ days/day_01/main.go
@@ -2,6 +2,8 @@ package day_01
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 )
 
 // Run function of the daily challenge
@@ -16,7 +18,25 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	sum := 0
+	for _, line := range input {
+		sum += firstInt(line)*10 + lastInt(line)
+	}
+	return strconv.Itoa(sum)
+}
+
+// firstInt gets the first integer in a given string
+func firstInt(s string) int {
+	re := regexp.MustCompile("^[a-z]*\\d")
+	match := re.FindString(s)
+	return int(match[len(match)-1]) - 48
+}
+
+// lastInt gets the last integer in a given string
+func lastInt(s string) int {
+	re := regexp.MustCompile("\\d[a-z]*$")
+	match := re.FindString(s)
+	return int(match[0]) - 48
 }
 
 // Part2 solves the second part of the exercise
