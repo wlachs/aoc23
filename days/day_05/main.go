276e848f135fd0775d7b952bd86a17c2e46dcf70
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-12-05 07:37:46 +0100

    non working part 2

--- days/day_05/main.go
+++ days/day_05/main.go
@@ -20,15 +20,15 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return strconv.Itoa(findLowestLocation(input))
+	seeds := getInitialSeeds(input)
+	return strconv.Itoa(findLowestLocation(seeds, input))
 }
 
 // findLowestLocation finds the lowest location number corresponding to any of the initial seeds
-func findLowestLocation(input []string) int {
-	seeds := getInitialSeeds(input)
+func findLowestLocation(seeds []int, input []string) int {
 	m := math.MaxInt
 	for _, seed := range seeds {
-		loc := getHumidity2Location(getTemperature2Humidity(getLight2Temperature(getWater2Light(getFertilizer2Water(getSoil2Fertilizer(getSeed2Soil(seed, input), input), input), input), input), input), input)
+		loc := getSeed2Soil(seed, input)
 		if loc < m {
 			m = loc
 		}
@@ -50,32 +50,32 @@ func getInitialSeeds(input []string) []int {
 
 // getSeed2Soil find the soil that corresponds to a given seed
 func getSeed2Soil(id int, input []string) int {
-	return getMappedValue(id, "seed-to-soil map:", input)
+	return getSoil2Fertilizer(getMappedValue(id, "seed-to-soil map:", input), input)
 }
 
 // getSoil2Fertilizer find the fertilizer that corresponds to a given soil
 func getSoil2Fertilizer(id int, input []string) int {
-	return getMappedValue(id, "soil-to-fertilizer map:", input)
+	return getFertilizer2Water(getMappedValue(id, "soil-to-fertilizer map:", input), input)
 }
 
 // getFertilizer2Water find the water that corresponds to a given fertilizer
 func getFertilizer2Water(id int, input []string) int {
-	return getMappedValue(id, "fertilizer-to-water map:", input)
+	return getWater2Light(getMappedValue(id, "fertilizer-to-water map:", input), input)
 }
 
 // getWater2Light find the light that corresponds to a given water
 func getWater2Light(id int, input []string) int {
-	return getMappedValue(id, "water-to-light map:", input)
+	return getLight2Temperature(getMappedValue(id, "water-to-light map:", input), input)
 }
 
 // getLight2Temperature find the temperature that corresponds to a given light
 func getLight2Temperature(id int, input []string) int {
-	return getMappedValue(id, "light-to-temperature map:", input)
+	return getTemperature2Humidity(getMappedValue(id, "light-to-temperature map:", input), input)
 }
 
 // getTemperature2Humidity find the humidity that corresponds to a given temperature
 func getTemperature2Humidity(id int, input []string) int {
-	return getMappedValue(id, "temperature-to-humidity map:", input)
+	return getHumidity2Location(getMappedValue(id, "temperature-to-humidity map:", input), input)
 }
 
 // getHumidity2Location find the location that corresponds to a given humidity
@@ -118,5 +118,21 @@ func getMappedValues(cat string, input []string) [][]int {
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	seeds := getExtendedSeeds(input)
+	return strconv.Itoa(findLowestLocation(seeds, input))
+}
+
+// getExtendedSeeds find the extended seeds from the input
+func getExtendedSeeds(input []string) []int {
+	splitter := regexp.MustCompile("\\s+")
+	seeds := splitter.Split(strings.Split(input[0], ": ")[1], -1)
+	var s []int
+	for i := 0; i < len(seeds); i += 2 {
+		start, _ := strconv.Atoi(seeds[i])
+		r, _ := strconv.Atoi(seeds[i+1])
+		for j := start; j < start+r; j++ {
+			s = append(s, j)
+		}
+	}
+	return s
 }
