23b8bd0fc5f80de8ac0aca83c48b578e3d6b3569
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-12-11 10:24:57 +0100

    part 1

--- days/day_11/main.go
+++ days/day_11/main.go
@@ -2,7 +2,15 @@ package day_11
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 )
+
+// coordinates define a pair of X Y values indicating the position on a 2D map
+type coordinates struct {
+	x int
+	y int
+}
 
 // Run function of the daily challenge
 func Run(input []string, mode int) {
@@ -16,10 +24,60 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	sum := 0
+	g := findGalaxies(input)
+	for _, g1 := range g {
+		for _, g2 := range g {
+			dx := float64(g2.x - g1.x)
+			dy := float64(g2.y - g1.y)
+			sum += int(math.Abs(dx) + math.Abs(dy))
+		}
+	}
+	return strconv.Itoa(sum / 2)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
 	return ""
+}
+
+// findGalaxies iterates over the input and finds the corrected location of each galaxy
+func findGalaxies(input []string) []coordinates {
+	var offset coordinates
+	var g []coordinates
+	for y, row := range input {
+		if rowEmpty(input, y) {
+			offset.y++
+		}
+		offset.x = 0
+		for x, c := range row {
+			if columnEmpty(input, x) {
+				offset.x++
+			}
+			if c == '#' {
+				g = append(g, coordinates{x + offset.x, y + offset.y})
+			}
+		}
+	}
+	return g
+}
+
+// columnEmpty checks whether the given column of the input contains no galaxies
+func columnEmpty(input []string, x int) bool {
+	for y := 0; y < len(input); y++ {
+		if input[y][x] == '#' {
+			return false
+		}
+	}
+	return true
+}
+
+// rowEmpty checks whether the given row of the input contains no galaxies
+func rowEmpty(input []string, y int) bool {
+	for _, c := range input[y] {
+		if c == '#' {
+			return false
+		}
+	}
+	return true
 }
