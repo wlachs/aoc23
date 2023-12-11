d907a984406dc540a91627f659f297ea32f8dff5
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-12-11 11:11:15 +0100

    part 2

--- days/day_11/main.go
+++ days/day_11/main.go
@@ -25,7 +25,7 @@ func Run(input []string, mode int) {
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
 	sum := 0
-	g := findGalaxies(input)
+	g := findGalaxies(input, 2)
 	for _, g1 := range g {
 		for _, g2 := range g {
 			dx := float64(g2.x - g1.x)
@@ -38,21 +38,30 @@ func Part1(input []string) string {
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	sum := 0
+	g := findGalaxies(input, 1000000)
+	for _, g1 := range g {
+		for _, g2 := range g {
+			dx := float64(g2.x - g1.x)
+			dy := float64(g2.y - g1.y)
+			sum += int(math.Abs(dx) + math.Abs(dy))
+		}
+	}
+	return strconv.Itoa(sum / 2)
 }
 
 // findGalaxies iterates over the input and finds the corrected location of each galaxy
-func findGalaxies(input []string) []coordinates {
+func findGalaxies(input []string, o int) []coordinates {
 	var offset coordinates
 	var g []coordinates
 	for y, row := range input {
 		if rowEmpty(input, y) {
-			offset.y++
+			offset.y += o - 1
 		}
 		offset.x = 0
 		for x, c := range row {
 			if columnEmpty(input, x) {
-				offset.x++
+				offset.x += o - 1
 			}
 			if c == '#' {
 				g = append(g, coordinates{x + offset.x, y + offset.y})
