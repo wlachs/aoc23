dbedd5ca79cbfd7e717cc15d9f341a9251ab8f83
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-12-13 16:21:12 +0100

    part 2

--- days/day_13/main.go
+++ days/day_13/main.go
@@ -2,6 +2,7 @@ package day_13
 
 import (
 	"fmt"
+	"slices"
 	"strconv"
 )
 
@@ -19,14 +20,18 @@ func Run(input []string, mode int) {
 func Part1(input []string) string {
 	sum := 0
 	for _, m := range getMaps(input) {
-		sum += calculateReflections(m)
+		sum += calculateReflections(m, -1)
 	}
 	return strconv.Itoa(sum)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	sum := 0
+	for _, m := range getMaps(input) {
+		sum += calculateReflectionsWithSmudge(m)
+	}
+	return strconv.Itoa(sum)
 }
 
 // getMaps reads the input and returns the separate maps as slice of string slices
@@ -45,27 +50,54 @@ func getMaps(input []string) [][]string {
 }
 
 // calculateReflections counts the amount of rows (above) and columns (left) reflected on the input map
-func calculateReflections(input []string) int {
-	return calculateColumnReflections(input) + 100*calculateRowReflections(input)
+func calculateReflections(input []string, exclude int) int {
+	r := calculateRowReflections(input, exclude/100)
+	if r > 0 {
+		return 100 * r
+	}
+	return calculateColumnReflections(input, exclude)
+}
+
+// calculateReflectionsWithSmudge counts the amount of rows (above) and columns (left) reflected on the input map
+// the only difference if the smudge guessing which tries to flip a single node and recalculate the mirror axis
+func calculateReflectionsWithSmudge(input []string) int {
+	oldRef := calculateReflections(input, -1)
+	for y, row := range input {
+		for x, c := range row {
+			if c == '#' {
+				c = '.'
+			} else {
+				c = '#'
+			}
+			newRow := input[y][:x] + string(c) + input[y][x+1:]
+			clone := slices.Clone(input)
+			clone[y] = newRow
+			ref := calculateReflections(clone, oldRef)
+			if ref > 0 && ref != oldRef {
+				return ref
+			}
+		}
+	}
+	panic("no smudge found")
 }
 
 // calculateColumnReflections calculates the number of columns reflected on the left side of the mirror
-func calculateColumnReflections(input []string) int {
+func calculateColumnReflections(input []string, exclude int) int {
 	possibleAxis := map[int]int{}
 	for _, row := range input {
 		stringAxis(row, possibleAxis)
 	}
-	return getAxis(possibleAxis, len(input))
+	return getAxis(possibleAxis, len(input), exclude)
 }
 
 // calculateRowReflections calculates the number of rows reflected above the mirror
-func calculateRowReflections(input []string) int {
+func calculateRowReflections(input []string, exclude int) int {
 	possibleAxis := map[int]int{}
 	for x := range input[0] {
 		column := getColumn(x, input)
 		stringAxis(column, possibleAxis)
 	}
-	return getAxis(possibleAxis, len(input[0]))
+	return getAxis(possibleAxis, len(input[0]), exclude)
 }
 
 // getColumn retrieves the selected column of the input
@@ -95,9 +127,9 @@ func stringAxis(s string, possibleAxis map[int]int) {
 }
 
 // getAxis finds the common axis within the possible mirrors
-func getAxis(possibleAxis map[int]int, length int) int {
+func getAxis(possibleAxis map[int]int, length int, exclude int) int {
 	for mirror, matches := range possibleAxis {
-		if matches == length {
+		if matches == length && mirror != exclude {
 			return mirror
 		}
 	}
