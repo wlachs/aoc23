f38061b19a00c298afc51a2e1da8f38506a4a537
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-12-15 09:28:12 +0100

    part 1

--- days/day_15/main.go
+++ days/day_15/main.go
@@ -2,6 +2,8 @@ package day_15
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 // Run function of the daily challenge
@@ -16,10 +18,24 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	sum := 0
+	for _, s := range strings.Split(input[0], ",") {
+		sum += int(hash(s))
+	}
+	return strconv.Itoa(sum)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
 	return ""
+}
+
+// hash calculates the hash of the given string
+// The hash is a numerical value from 0 to 255
+func hash(s string) uint8 {
+	h := uint8(0)
+	for _, c := range s {
+		h = 17 * (h + uint8(c))
+	}
+	return h
 }
