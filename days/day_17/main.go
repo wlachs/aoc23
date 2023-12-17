22f35ceae90017a16ef905ff6b533e130738c982
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-12-18 00:11:37 +0100

    part 2

--- days/day_17/main.go
+++ days/day_17/main.go
@@ -25,7 +25,6 @@ type PosDirRem struct {
 type Record struct {
 	PosDirRem
 	weight int
-	path   []types.Vec2
 }
 
 // Run function of the daily challenge
@@ -43,13 +42,17 @@ func Part1(input []string) string {
 	m := utils.ParseInputToMap(input)
 	mem := map[types.Vec2]int{}
 	br := bottomRight(m)
-	findShortestPath(m, mem, &br)
+	findShortestPathV1(m, mem, &br)
 	return strconv.Itoa(mem[br])
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	m := utils.ParseInputToMap(input)
+	mem := map[types.Vec2]int{}
+	br := bottomRight(m)
+	findShortestPathV2(m, mem, &br)
+	return strconv.Itoa(mem[br])
 }
 
 // bottomRight finds the element at the bottom right position of the map
@@ -62,17 +65,45 @@ func bottomRight(m map[types.Vec2]int32) types.Vec2 {
 	return r
 }
 
-// findShortestPath navigates through the map while trying to find the path from top left to bottom right with the minimal possible heat loss
-func findShortestPath(m map[types.Vec2]int32, mem map[types.Vec2]int, br *types.Vec2) {
+// findShortestPathV1 navigates through the map while trying to find the path from top left to bottom right with the minimal possible heat loss
+// part 1
+func findShortestPathV1(m map[types.Vec2]int32, mem map[types.Vec2]int, br *types.Vec2) {
 	nodes := map[PosDirRem]int{
 		{pos: types.Vec2{}, DirRem: DirRem{dir: types.Vec2{X: 1}, rem: 3}}: mem[types.Vec2{}],
+		{pos: types.Vec2{}, DirRem: DirRem{dir: types.Vec2{Y: 1}, rem: 3}}: mem[types.Vec2{}],
 	}
 	history := map[types.Vec2][]DirRem{}
 	for len(nodes) > 0 {
 		current := getNextNode(nodes)
 		delete(nodes, current.PosDirRem)
 		history[current.pos] = append(history[current.pos], current.DirRem)
-		neighbours := getNeighbours(m, mem, current, br)
+		neighbours := getNeighboursV1(m, mem, current, br)
+		for _, record := range neighbours {
+			if !slices.Contains(history[record.pos], record.DirRem) {
+				w, ok := nodes[record.PosDirRem]
+				if ok {
+					nodes[record.PosDirRem] = min(w, record.weight)
+				} else {
+					nodes[record.PosDirRem] = record.weight
+				}
+			}
+		}
+	}
+}
+
+// findShortestPathV2 navigates through the map while trying to find the path from top left to bottom right with the minimal possible heat loss
+// part 2
+func findShortestPathV2(m map[types.Vec2]int32, mem map[types.Vec2]int, br *types.Vec2) {
+	nodes := map[PosDirRem]int{
+		{pos: types.Vec2{}, DirRem: DirRem{dir: types.Vec2{X: 1}, rem: 10}}: mem[types.Vec2{}],
+		{pos: types.Vec2{}, DirRem: DirRem{dir: types.Vec2{Y: 1}, rem: 10}}: mem[types.Vec2{}],
+	}
+	history := map[types.Vec2][]DirRem{}
+	for len(nodes) > 0 {
+		current := getNextNode(nodes)
+		delete(nodes, current.PosDirRem)
+		history[current.pos] = append(history[current.pos], current.DirRem)
+		neighbours := getNeighboursV2(m, mem, current, br)
 		for _, record := range neighbours {
 			if !slices.Contains(history[record.pos], record.DirRem) {
 				w, ok := nodes[record.PosDirRem]
@@ -104,18 +135,19 @@ func weight(m map[types.Vec2]int32, pos *types.Vec2) int {
 	return int(m[*pos] - '0')
 }
 
-// getNeighbours gets the shortest path from the current node to its neighbours
-func getNeighbours(m map[types.Vec2]int32, mem map[types.Vec2]int, node *Record, br *types.Vec2) []Record {
+// getNeighboursV1 gets the shortest path from the current node to its neighbours
+// can only move 3 block straight in a row
+func getNeighboursV1(m map[types.Vec2]int32, mem map[types.Vec2]int, node *Record, br *types.Vec2) []Record {
 	var res []Record
 	l := node.pos.Around()
 	for _, v := range l {
 		if node.pos.Subtract(&v) != node.dir {
 			dir := v.Subtract(&node.pos)
-			rem := 2
+			rem := 3
 			if dir == node.dir {
 				rem = node.rem - 1
 			}
-			if rem >= 0 && inBounds(&v, br) {
+			if rem > 0 && inBounds(&v, br) {
 				w := node.weight + weight(m, &v)
 				oldW, ok := mem[v]
 				rec := Record{
@@ -127,7 +159,6 @@ func getNeighbours(m map[types.Vec2]int32, mem map[types.Vec2]int, node *Record,
 						},
 					},
 					weight: w,
-					path:   slices.Clone(node.path),
 				}
 				if ok {
 					mem[v] = min(oldW, w)
@@ -141,15 +172,46 @@ func getNeighbours(m map[types.Vec2]int32, mem map[types.Vec2]int, node *Record,
 	return res
 }
 
-func printSteps(mem map[types.Vec2]int) {
-	for y := 0; y <= 12; y++ {
-		fmt.Print("| ")
-		for x := 0; x <= 12; x++ {
-			fmt.Printf(" %03d |", mem[types.Vec2{X: x, Y: y}])
-		}
-		fmt.Println()
+// getNeighboursV2 gets the shortest path from the current node to its neighbours
+// must move at least 4 blocks straight but not more than 10
+func getNeighboursV2(m map[types.Vec2]int32, mem map[types.Vec2]int, node *Record, br *types.Vec2) []Record {
+	var res []Record
+	var vectors []types.Vec2
+	if node.rem > 7 {
+		vectors = append(vectors, node.pos.Add(&node.dir))
+	} else {
+		vectors = node.pos.Around()
 	}
-	fmt.Println()
+	for _, v := range vectors {
+		if node.pos.Subtract(&v) != node.dir {
+			dir := v.Subtract(&node.pos)
+			rem := 10
+			if dir == node.dir {
+				rem = node.rem - 1
+			}
+			if rem > 0 && inBounds(&v, br) && (v != *br || rem <= 7) {
+				w := node.weight + weight(m, &v)
+				oldW, ok := mem[v]
+				rec := Record{
+					PosDirRem: PosDirRem{
+						pos: v,
+						DirRem: DirRem{
+							dir: dir,
+							rem: rem,
+						},
+					},
+					weight: w,
+				}
+				if ok {
+					mem[v] = min(oldW, w)
+				} else {
+					mem[v] = w
+				}
+				res = append(res, rec)
+			}
+		}
+	}
+	return res
 }
 
 // inBounds makes sure that the current location is within the map coordinates
