2295661148c4d153836647e906524a121d5902e2
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-12-20 16:12:04 +0100

    part 1

--- days/day_20/main.go
+++ days/day_20/main.go
@@ -2,7 +2,163 @@ package day_20
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
+
+// MessageQueueEntry holds a tuple of Module pointer and input pulse
+type MessageQueueEntry struct {
+	source Module
+	target Module
+	pulse  bool
+}
+
+// MessageQueue interface
+type MessageQueue interface {
+	add(entry MessageQueueEntry)
+	pop() MessageQueueEntry
+}
+
+// messageQueue holding entries to process
+type messageQueue struct {
+	entries   []MessageQueueEntry
+	lowCount  int
+	highCount int
+}
+
+// add method adds a new MessageQueueEntry to the MessageQueue
+func (receiver *messageQueue) add(entry MessageQueueEntry) {
+	receiver.entries = append(receiver.entries, entry)
+	if entry.pulse {
+		receiver.highCount++
+	} else {
+		receiver.lowCount++
+	}
+}
+
+// pop removes the first entry of the queue and returns it
+func (receiver *messageQueue) pop() MessageQueueEntry {
+	e := receiver.entries[0]
+	receiver.entries = receiver.entries[1:]
+	return e
+}
+
+// Module is the general module interface implemented by all module types
+type Module interface {
+	pre(p bool, src Module)
+	pulse(p bool, src Module)
+	addInput(m Module)
+	addOutput(m Module)
+}
+
+// FlipFlop module with an internal state
+type FlipFlop struct {
+	name         string
+	state        bool
+	messageQueue MessageQueue
+	outputNodes  []Module
+}
+
+// pre is responsible for preparing the module for pulsing
+func (receiver *FlipFlop) pre(_ bool, _ Module) {
+	//
+}
+
+// pulse handler of a FlipFlop module
+func (receiver *FlipFlop) pulse(p bool, _ Module) {
+	if p {
+		return
+	}
+	receiver.state = !receiver.state
+	for _, module := range receiver.outputNodes {
+		if module != nil {
+			module.pre(receiver.state, receiver)
+		}
+		receiver.messageQueue.add(MessageQueueEntry{source: receiver, target: module, pulse: receiver.state})
+	}
+}
+
+// addInput adds the given node to the current node's input queue
+func (receiver *FlipFlop) addInput(m Module) {
+	//
+}
+
+// addOutput adds the given node to the current node's output queue
+func (receiver *FlipFlop) addOutput(m Module) {
+	receiver.outputNodes = append(receiver.outputNodes, m)
+}
+
+// Conjunction module with upstream input memory
+type Conjunction struct {
+	name         string
+	inputMemory  map[Module]bool
+	messageQueue MessageQueue
+	outputNodes  []Module
+}
+
+// pre is responsible for preparing the module for pulsing
+func (receiver *Conjunction) pre(p bool, src Module) {
+	receiver.inputMemory[src] = p
+}
+
+// pulse handler of a Conjunction module
+func (receiver *Conjunction) pulse(_ bool, _ Module) {
+	out := false
+	for _, b := range receiver.inputMemory {
+		if !b {
+			out = true
+			break
+		}
+	}
+	for _, module := range receiver.outputNodes {
+		if module != nil {
+			module.pre(out, receiver)
+		}
+		receiver.messageQueue.add(MessageQueueEntry{source: receiver, target: module, pulse: out})
+	}
+}
+
+// addInput adds the given node to the current node's input queue
+func (receiver *Conjunction) addInput(m Module) {
+	receiver.inputMemory[m] = false
+}
+
+// addOutput adds the given node to the current node's output queue
+func (receiver *Conjunction) addOutput(m Module) {
+	receiver.outputNodes = append(receiver.outputNodes, m)
+}
+
+// Broadcaster module, sends the input to all outputs
+type Broadcaster struct {
+	messageQueue MessageQueue
+	outputNodes  []Module
+}
+
+// pre is responsible for preparing the module for pulsing
+func (receiver *Broadcaster) pre(_ bool, _ Module) {
+	//
+}
+
+// pulse handler of the Broadcaster module
+func (receiver *Broadcaster) pulse(p bool, _ Module) {
+	for _, module := range receiver.outputNodes {
+		if module != nil {
+			module.pre(p, receiver)
+		}
+		receiver.messageQueue.add(MessageQueueEntry{source: receiver, target: module, pulse: p})
+	}
+}
+
+// addInput adds the given node to the current node's input queue
+func (receiver *Broadcaster) addInput(m Module) {
+	//
+}
+
+// addOutput adds the given node to the current node's output queue
+func (receiver *Broadcaster) addOutput(m Module) {
+	receiver.outputNodes = append(receiver.outputNodes, m)
+}
 
 // Run function of the daily challenge
 func Run(input []string, mode int) {
@@ -16,10 +172,78 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	mq, broadcaster := parseInput(input)
+	for i := 0; i < 1000; i++ {
+		mq.add(MessageQueueEntry{
+			target: broadcaster,
+			pulse:  false,
+		})
+		for len(mq.entries) > 0 {
+			e := mq.pop()
+			if e.target != nil {
+				e.target.pulse(e.pulse, e.source)
+			}
+		}
+	}
+	return strconv.Itoa(mq.lowCount * mq.highCount)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
 	return ""
+}
+
+// parseInput sets up the system with the input nodes
+func parseInput(input []string) (*messageQueue, *Broadcaster) {
+	mq := messageQueue{
+		entries:   []MessageQueueEntry{},
+		lowCount:  0,
+		highCount: 0,
+	}
+	moduleMap := map[string]Module{}
+	flipFlops := map[string]FlipFlop{}
+	conjunctions := map[string]Conjunction{}
+	broadcaster := Broadcaster{
+		messageQueue: &mq,
+		outputNodes:  []Module{},
+	}
+	moduleMap["broadcaster"] = &broadcaster
+	for _, row := range input {
+		ff := regexp.MustCompile(`%(?P<a>\w+)`).FindStringSubmatch(row)
+		if len(ff) > 0 {
+			m := FlipFlop{
+				name:         ff[1],
+				state:        false,
+				messageQueue: &mq,
+				outputNodes:  []Module{},
+			}
+			moduleMap[ff[1]] = &m
+			flipFlops[ff[1]] = m
+			continue
+		}
+		c := regexp.MustCompile(`&(?P<a>\w+)`).FindStringSubmatch(row)
+		if len(c) > 0 {
+			m := Conjunction{
+				name:         c[1],
+				inputMemory:  map[Module]bool{},
+				messageQueue: &mq,
+				outputNodes:  []Module{},
+			}
+			moduleMap[c[1]] = &m
+			conjunctions[c[1]] = m
+			continue
+		}
+	}
+	for _, row := range input {
+		r := regexp.MustCompile(`^[%&]*(?P<a>\w+) -> (?P<b>.+$)`).FindStringSubmatch(row)
+		src := moduleMap[r[1]]
+		for _, t := range strings.Split(r[2], ", ") {
+			target := moduleMap[t]
+			src.addOutput(target)
+			if target != nil {
+				target.addInput(src)
+			}
+		}
+	}
+	return &mq, &broadcaster
 }
