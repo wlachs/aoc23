0984e28f9e2e094c13acb5e954f9555e24d1cedb
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-01-16 22:23:38 +0100

    part 2 slow

--- days/day_23/main.go
+++ days/day_23/main.go
@@ -24,7 +24,7 @@ func Part1(input []string) string {
 	for x := 0; x < len(input[0]); x++ {
 		pos := types.Vec2{X: x}
 		if m[pos] == '.' {
-			return strconv.Itoa(findLongestPath(m, []types.Vec2{pos}, types.Vec2{X: len(input[0]), Y: len(input)}))
+			return strconv.Itoa(findLongestPath(m, []types.Vec2{pos}, types.Vec2{X: len(input[0]), Y: len(input)}, false))
 		}
 	}
 	panic("no starting field found")
@@ -32,28 +32,48 @@ func Part1(input []string) string {
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	m := utils.ParseInputToMap(input)
+	for x := 0; x < len(input[0]); x++ {
+		pos := types.Vec2{X: x}
+		if m[pos] == '.' {
+			return strconv.Itoa(findLongestPath(m, []types.Vec2{pos}, types.Vec2{X: len(input[0]), Y: len(input)}, true))
+		}
+	}
+	panic("no starting field found")
 }
 
+var best = 0
+
 // findLongestPath iterates over the input map and finds the longest hiking path without loops
-func findLongestPath(m map[types.Vec2]int32, path []types.Vec2, dim types.Vec2) int {
+func findLongestPath(m map[types.Vec2]int32, path []types.Vec2, dim types.Vec2, ignoreSlopes bool) int {
 	var options []types.Vec2
 	for len(options) < 2 {
 		if len(options) == 1 {
 			path = append(path, options[0])
 		}
-		options = findNextOptions(m, path, dim)
+		if ignoreSlopes {
+			options = findNonSlipperyNextOptions(m, path, dim)
+		} else {
+			options = findNextOptions(m, path, dim)
+		}
 		if len(options) == 0 {
-			return len(path) - 1
+			if path[len(path)-1].Y == dim.Y-1 {
+				return len(path) - 1
+			}
+			return 0
 		}
 	}
-	lengths := make([]int, len(options))
-	for i, option := range options {
+	longest := 0
+	for _, option := range options {
 		newPath := slices.Clone(path)
 		newPath = append(newPath, option)
-		lengths[i] = findLongestPath(m, newPath, dim)
+		longest = max(longest, findLongestPath(m, newPath, dim, ignoreSlopes))
+		if longest > best {
+			best = longest
+			fmt.Println(best)
+		}
 	}
-	return slices.Max(lengths)
+	return longest
 }
 
 // findNextOptions checks the surrounding fields for the next step of the hike
@@ -74,6 +94,30 @@ func findNextOptions(m map[types.Vec2]int32, path []types.Vec2, dim types.Vec2) 
 	}
 	right := pos.Right()
 	if right.X < dim.X && m[right] != '<' && m[right] != '#' && !slices.Contains(path, right) {
+		options = append(options, right)
+	}
+	return options
+}
+
+// findNonSlipperyNextOptions checks the surrounding fields for the next step of the hike
+// this variant of the method ignores steep slopes
+func findNonSlipperyNextOptions(m map[types.Vec2]int32, path []types.Vec2, dim types.Vec2) []types.Vec2 {
+	options := make([]types.Vec2, 0, 4)
+	pos := path[len(path)-1]
+	up := pos.Up()
+	if up.Y >= 0 && m[up] != '#' && !slices.Contains(path, up) {
+		options = append(options, up)
+	}
+	down := pos.Down()
+	if down.Y < dim.Y && m[down] != '#' && !slices.Contains(path, down) {
+		options = append(options, down)
+	}
+	left := pos.Left()
+	if left.X >= 0 && m[left] != '#' && !slices.Contains(path, left) {
+		options = append(options, left)
+	}
+	right := pos.Right()
+	if right.X < dim.X && m[right] != '#' && !slices.Contains(path, right) {
 		options = append(options, right)
 	}
 	return options
