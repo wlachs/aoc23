0a1e77e20a0673870bd8bff9e6decd9fb03d74b0
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-01-21 21:53:04 +0100

    part 2

--- days/day_24/main.go
+++ days/day_24/main.go
@@ -17,8 +17,8 @@ type RatVec3 struct {
 }
 
 // Add adds two vectors
-func (vec *RatVec3) Add(other *RatVec3) RatVec3 {
-	return RatVec3{
+func (vec *RatVec3) Add(other *RatVec3) *RatVec3 {
+	return &RatVec3{
 		X: add(vec.X, other.X),
 		Y: add(vec.Y, other.Y),
 		Z: add(vec.Z, other.Z),
@@ -26,8 +26,8 @@ func (vec *RatVec3) Add(other *RatVec3) RatVec3 {
 }
 
 // Subtract subtracts two vectors
-func (vec *RatVec3) Subtract(other *RatVec3) RatVec3 {
-	return RatVec3{
+func (vec *RatVec3) Subtract(other *RatVec3) *RatVec3 {
+	return &RatVec3{
 		X: sub(vec.X, other.X),
 		Y: sub(vec.Y, other.Y),
 		Z: sub(vec.Z, other.Z),
@@ -35,8 +35,8 @@ func (vec *RatVec3) Subtract(other *RatVec3) RatVec3 {
 }
 
 // Multiply multiplies the vector with the given scalar
-func (vec *RatVec3) Multiply(s *big.Rat) RatVec3 {
-	return RatVec3{
+func (vec *RatVec3) Multiply(s *big.Rat) *RatVec3 {
+	return &RatVec3{
 		X: mul(vec.X, s),
 		Y: mul(vec.Y, s),
 		Z: mul(vec.Z, s),
@@ -44,8 +44,8 @@ func (vec *RatVec3) Multiply(s *big.Rat) RatVec3 {
 }
 
 // Divide divides the vector with the given scalar
-func (vec *RatVec3) Divide(s *big.Rat) RatVec3 {
-	return RatVec3{
+func (vec *RatVec3) Divide(s *big.Rat) *RatVec3 {
+	return &RatVec3{
 		X: quo(vec.X, s),
 		Y: quo(vec.Y, s),
 		Z: quo(vec.Z, s),
@@ -53,8 +53,8 @@ func (vec *RatVec3) Divide(s *big.Rat) RatVec3 {
 }
 
 // Cross calculates the cross product of two vectors
-func (vec *RatVec3) Cross(other *RatVec3) RatVec3 {
-	return RatVec3{
+func (vec *RatVec3) Cross(other *RatVec3) *RatVec3 {
+	return &RatVec3{
 		X: sub(mul(vec.Y, other.Z), mul(vec.Z, other.Y)),
 		Y: sub(mul(vec.Z, other.X), mul(vec.X, other.Z)),
 		Z: sub(mul(vec.X, other.Y), mul(vec.Y, other.X)),
@@ -69,15 +69,15 @@ func (vec *RatVec3) Dot(other *RatVec3) *big.Rat {
 // HailStone represents a pair of 3D vectors.
 // The p0 vector holds the initial position of the hailstone, whereas the v vector holds its direction
 type HailStone struct {
-	p RatVec3
-	v RatVec3
+	p *RatVec3
+	v *RatVec3
 }
 
 // Subtract subtracts both vectors of a HailStone from another HailStone
-func (stone HailStone) Subtract(other HailStone) HailStone {
-	return HailStone{
-		p: stone.p.Subtract(&other.p),
-		v: stone.v.Subtract(&other.v),
+func (stone *HailStone) Subtract(other *HailStone) *HailStone {
+	return &HailStone{
+		p: stone.p.Subtract(other.p),
+		v: stone.v.Subtract(other.v),
 	}
 }
 
@@ -100,8 +100,22 @@ func Part1(input []string) string {
 }
 
 // Part2 solves the second part of the exercise
+// Solution inspired by https://aidiakapi.com/blog/2024-01-20-advent-of-code-2023-day-24/
 func Part2(input []string) string {
-	return ""
+	hailStones := readInput(input[:3])
+	s1 := hailStones[1]
+	s2 := hailStones[2]
+	ref1 := s1.Subtract(&hailStones[0])
+	ref2 := s2.Subtract(&hailStones[0])
+
+	t1 := intersectionTime(ref2, ref1)
+	t2 := intersectionTime(ref1, ref2)
+
+	rock1 := s1.p.Add(s1.v.Multiply(t1))
+	rock2 := s2.p.Add(s2.v.Multiply(t2))
+
+	rp := rock1.Subtract(rock2.Subtract(rock1).Divide(sub(t2, t1)).Multiply(t1))
+	return add(rp.X, add(rp.Y, rp.Z)).FloatString(0)
 }
 
 // readInput reads the input and creates a slice of hailstones from it
@@ -125,12 +139,12 @@ func readLine(line string) HailStone {
 	}
 
 	return HailStone{
-		p: RatVec3{
+		p: &RatVec3{
 			X: big.NewRat(int64(m[0]), 1),
 			Y: big.NewRat(int64(m[1]), 1),
 			Z: big.NewRat(int64(m[2]), 1),
 		},
-		v: RatVec3{
+		v: &RatVec3{
 			X: big.NewRat(int64(m[3]), 1),
 			Y: big.NewRat(int64(m[4]), 1),
 			Z: big.NewRat(int64(m[5]), 1),
@@ -172,8 +186,9 @@ func doMatch2D(a *HailStone, b *HailStone) (*big.Rat, *big.Rat, *big.Rat, *big.R
 }
 
 // intersectionTime calculates the time of intersection of two hailstones.
-func intersectionTime(r *HailStone, s *HailStone) float64 {
-	return float64(0)
+func intersectionTime(r *HailStone, s *HailStone) *big.Rat {
+	plane := r.p.Cross(r.p.Add(r.v))
+	return quo(mul(s.p.Dot(plane), big.NewRat(-1, 1)), s.v.Dot(plane))
 }
 
 // r copies a big.Rat number
