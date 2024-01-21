35f95a89c11ea203ecdcb5b37be72c262a46f99f
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-01-21 21:35:11 +0100

    use big.Rat for precise calculations

--- days/day_24/main.go
+++ days/day_24/main.go
@@ -2,22 +2,83 @@ package day_24
 
 import (
 	"fmt"
+	"github.com/wlchs/advent_of_code_go_template/utils"
+	"math/big"
 	"regexp"
 	"strconv"
 )
 
-// FloatVec3 is the float64 implementation of the Vec3 type
-type FloatVec3 struct {
-	X float64
-	Y float64
-	Z float64
+var zero = big.NewRat(0, 1)
+
+type RatVec3 struct {
+	X *big.Rat
+	Y *big.Rat
+	Z *big.Rat
+}
+
+// Add adds two vectors
+func (vec *RatVec3) Add(other *RatVec3) RatVec3 {
+	return RatVec3{
+		X: add(vec.X, other.X),
+		Y: add(vec.Y, other.Y),
+		Z: add(vec.Z, other.Z),
+	}
+}
+
+// Subtract subtracts two vectors
+func (vec *RatVec3) Subtract(other *RatVec3) RatVec3 {
+	return RatVec3{
+		X: sub(vec.X, other.X),
+		Y: sub(vec.Y, other.Y),
+		Z: sub(vec.Z, other.Z),
+	}
+}
+
+// Multiply multiplies the vector with the given scalar
+func (vec *RatVec3) Multiply(s *big.Rat) RatVec3 {
+	return RatVec3{
+		X: mul(vec.X, s),
+		Y: mul(vec.Y, s),
+		Z: mul(vec.Z, s),
+	}
+}
+
+// Divide divides the vector with the given scalar
+func (vec *RatVec3) Divide(s *big.Rat) RatVec3 {
+	return RatVec3{
+		X: quo(vec.X, s),
+		Y: quo(vec.Y, s),
+		Z: quo(vec.Z, s),
+	}
+}
+
+// Cross calculates the cross product of two vectors
+func (vec *RatVec3) Cross(other *RatVec3) RatVec3 {
+	return RatVec3{
+		X: sub(mul(vec.Y, other.Z), mul(vec.Z, other.Y)),
+		Y: sub(mul(vec.Z, other.X), mul(vec.X, other.Z)),
+		Z: sub(mul(vec.X, other.Y), mul(vec.Y, other.X)),
+	}
+}
+
+// Dot calculates the dot product of two vectors
+func (vec *RatVec3) Dot(other *RatVec3) *big.Rat {
+	return add(mul(vec.X, other.X), add(mul(vec.Y, other.Y), mul(vec.Z, other.Z)))
 }
 
 // HailStone represents a pair of 3D vectors.
 // The p0 vector holds the initial position of the hailstone, whereas the v vector holds its direction
 type HailStone struct {
-	p0 FloatVec3
-	v  FloatVec3
+	p RatVec3
+	v RatVec3
+}
+
+// Subtract subtracts both vectors of a HailStone from another HailStone
+func (stone HailStone) Subtract(other HailStone) HailStone {
+	return HailStone{
+		p: stone.p.Subtract(&other.p),
+		v: stone.v.Subtract(&other.v),
+	}
 }
 
 // Run function of the daily challenge
@@ -33,8 +94,8 @@ func Run(input []string, mode int) {
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
 	hailStones := readInput(input)
-	rangeStart := float64(200000000000000)
-	rangeEnd := float64(400000000000000)
+	rangeStart := big.NewRat(200000000000000, 1)
+	rangeEnd := big.NewRat(400000000000000, 1)
 	return strconv.Itoa(countIntersections(hailStones, rangeStart, rangeEnd))
 }
 
@@ -58,34 +119,33 @@ func readInput(input []string) []HailStone {
 func readLine(line string) HailStone {
 	re := regexp.MustCompile(`-*\d+`)
 	match := re.FindAllString(line, -1)
-	m := make([]float64, 6)
+	m := make([]int, 6)
 	for i, s := range match {
-		f, _ := strconv.ParseFloat(s, 64)
-		m[i] = f
+		m[i] = utils.Atoi(s)
 	}
 
 	return HailStone{
-		p0: FloatVec3{
-			X: m[0],
-			Y: m[1],
-			Z: m[2],
+		p: RatVec3{
+			X: big.NewRat(int64(m[0]), 1),
+			Y: big.NewRat(int64(m[1]), 1),
+			Z: big.NewRat(int64(m[2]), 1),
 		},
-		v: FloatVec3{
-			X: m[3],
-			Y: m[4],
-			Z: m[5],
+		v: RatVec3{
+			X: big.NewRat(int64(m[3]), 1),
+			Y: big.NewRat(int64(m[4]), 1),
+			Z: big.NewRat(int64(m[5]), 1),
 		},
 	}
 }
 
 // countIntersections counts how many if the given hailstones intersect within the given coordinate range
-func countIntersections(stones []HailStone, start float64, end float64) int {
+func countIntersections(stones []HailStone, start *big.Rat, end *big.Rat) int {
 	count := 0
 
 	for i := range stones {
 		for j := range stones[i+1:] {
 			x, y, t1, t2, ok := doMatch2D(&stones[i], &stones[i+j+1])
-			if ok && t1 >= 0 && t2 >= 0 && x >= start && y >= start && x <= end && y <= end {
+			if ok && t1.Cmp(zero) > 0 && t2.Cmp(zero) >= 0 && x.Cmp(start) >= 0 && y.Cmp(start) >= 0 && x.Cmp(end) <= 0 && y.Cmp(end) <= 0 {
 				count++
 			}
 		}
@@ -95,22 +155,52 @@ func countIntersections(stones []HailStone, start float64, end float64) int {
 }
 
 // doMatch2D checks at which 2D location do the two given hailstones intersect with each other
-func doMatch2D(a *HailStone, b *HailStone) (float64, float64, float64, float64, bool) {
-	m1 := a.v.Y / a.v.X
-	m2 := b.v.Y / b.v.X
-	x1 := a.p0.X
-	y1 := a.p0.Y
-	x2 := b.p0.X
-	y2 := b.p0.Y
+func doMatch2D(a *HailStone, b *HailStone) (*big.Rat, *big.Rat, *big.Rat, *big.Rat, bool) {
+	m1 := quo(a.v.Y, a.v.X)
+	m2 := quo(b.v.Y, b.v.X)
 
-	if m1 == m2 {
-		return 0, 0, 0, 0, false
+	if m1.Cmp(m2) == 0 {
+		return nil, nil, nil, nil, false
 	}
 
-	x := ((m1*x1 - y1) - (m2*x2 - y2)) / (m1 - m2)
-	y := m1*(x-x1) + y1
-	t1 := (x - x1) / a.v.X
-	t2 := (x - x2) / b.v.X
+	x := quo(sub(sub(mul(m1, a.p.X), a.p.Y), sub(mul(m2, b.p.X), b.p.Y)), sub(m1, m2))
+	y := add(mul(m1, sub(x, a.p.X)), a.p.Y)
+	t1 := quo(sub(x, a.p.X), a.v.X)
+	t2 := quo(sub(x, b.p.X), b.v.X)
 
 	return x, y, t1, t2, true
+}
+
+// intersectionTime calculates the time of intersection of two hailstones.
+func intersectionTime(r *HailStone, s *HailStone) float64 {
+	return float64(0)
+}
+
+// r copies a big.Rat number
+func r(rat *big.Rat) *big.Rat {
+	return new(big.Rat).Set(rat)
+}
+
+// add adds two big.Rat numbers and returns the result without changing the original values
+func add(a *big.Rat, b *big.Rat) *big.Rat {
+	c := r(a)
+	return c.Add(c, b)
+}
+
+// sub subtracts big.Rat from another and returns the result without changing the original values
+func sub(a *big.Rat, b *big.Rat) *big.Rat {
+	c := r(a)
+	return c.Sub(c, b)
+}
+
+// mul multiplies two big.Rat numbers and returns the result without changing the original values
+func mul(a *big.Rat, b *big.Rat) *big.Rat {
+	c := r(a)
+	return c.Mul(c, b)
+}
+
+// quo divides big.Rat by another one and returns the result without changing the original values
+func quo(a *big.Rat, b *big.Rat) *big.Rat {
+	c := r(a)
+	return c.Quo(c, b)
 }
